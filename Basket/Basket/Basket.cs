00189fa5648454ead735c038dba18d73370707d6
tpauchard <t.pauchard@hokoha.com>
2020-06-09 16:05:09 +0200

    First commit

---
+++ Basket/Basket/Basket.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Basket
+{
+    public class Basket : BindableBase
+    {
+        public Basket()
+        {
+            Products = new ObservableCollection<Product>();
+        }
+        ObservableCollection<Product> _Products;
+        public ObservableCollection<Product> Products { get => _Products; set => SetProperty(ref _Products, value); }
+
+        public int Total { get => GetTotal(); }
+
+        public int GetTotal()
+        {
+            var total = 0;
+            foreach (var product in Products)
+            {
+                total += product.Value;
+            }
+            return total;
+        }
+    }
+
+    public class Product : BindableBase
+    {
+        public Product(int value)
+        {
+            Value = value;
+        }
+        int _Value;
+        public int Value
+        {
+            get => _Value; set
+            {
+                SetProperty(ref _Value, value);
+            }
+        }
+    }
+}
---
+++ Basket/Basket/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket
+{
+    public class MainPageViewModel : BindableBase
+    {
+        Basket _Basket;
+        public Basket Basket { get => _Basket; set => SetProperty(ref _Basket,value); }
+        public MainPageViewModel()
+        {
+            Basket = new Basket();
+            Product product1 = new Product(10);
+            Product product2 = new Product(5);
+            Basket.Products.Add(product1);
+            Basket.Products.Add(product2);
+        }
+    }
+}
